2e03f9e3ec6379428a6936e5f1e9b219a49b0402
Kelvin Li <jpkelvinli@gmail.com>
2018-05-09 14:25:21 -0700

    Change index lib name.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import h5c from './src/main';
+import hc5 from './src/main';
 
 var root = typeof self == 'object' && self.self === self && self ||
            typeof global == 'object' && global.global === global && global ||
            this;
 
-root.h5c = h5c;
+root.hc5 = hc5;
